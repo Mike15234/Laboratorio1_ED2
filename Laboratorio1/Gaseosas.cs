d7f2861d66f99a22a196300593aea6a251f7aed4
Mike15234 <miguelantonio1852@gmail.com>
2020-02-02 14:44:57 -0500

    Clase Gaseosas

--- Laboratorio1/Gaseosas.cs
+++ Laboratorio1/Gaseosas.cs
@@ -7,5 +7,14 @@ namespace Laboratorio1
 {
     public class Gaseosas
     {
+        public string Nombre { get; set; }
+        public string Sabor { get; set; }
+        public string Volumen { get; set; }
+        
+
+        public static Comparison<Gaseosas> ComparebyName = delegate (Gaseosas m1, Gaseosas m2)
+        {
+            return m1.Nombre.CompareTo(m2.Nombre);
+        };
     }
 }
